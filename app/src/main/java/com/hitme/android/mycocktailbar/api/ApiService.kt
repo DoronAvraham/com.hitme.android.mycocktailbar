d58d038b087f43c0cc5804e56a6e4a7e95d1bd78
doron.avraham <doron.avraham@avast.com>
2022-12-26 15:59:56 +0100

    Add custom HttpClient logger

--- app/src/main/java/com/hitme/android/mycocktailbar/api/ApiService.kt
+++ app/src/main/java/com/hitme/android/mycocktailbar/api/ApiService.kt
@@ -1,11 +1,15 @@
 package com.hitme.android.mycocktailbar.api
 
+import android.util.Log
+import com.hitme.android.mycocktailbar.BuildConfig
 import com.hitme.android.mycocktailbar.data.Cocktail
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.MessageLengthLimitingLogger
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.json.Json
@@ -24,6 +28,8 @@ interface ApiService {
         const val KEY_SEARCH = "s"
         const val KEY_SEARCH_BY_ID = "i"
 
+        const val LOGGER_TAG = "HttpClientLog"
+
         fun createClient(): HttpClient {
             return HttpClient(Android) {
 
@@ -38,8 +44,20 @@ interface ApiService {
 
                 install(Logging) {
                     level = LogLevel.ALL
+                    if (BuildConfig.DEBUG) {
+                        logger = Logger.CUSTOM
+                    }
                 }
             }
         }
+
+        private val Logger.Companion.CUSTOM: Logger
+            get() = MessageLengthLimitingLogger(
+                delegate = object : Logger {
+                    override fun log(message: String) {
+                        Log.d(LOGGER_TAG, message)
+                    }
+                }
+            )
     }
 }
