b71dc2bdc102955b0f153c3e95da24851b280377
doron.avraham <doron.avraham@avast.com>
2023-01-04 18:16:20 +0100

    Add StrictMode rules.

--- app/src/main/java/com/hitme/android/mycocktailbar/MainApplication.kt
+++ app/src/main/java/com/hitme/android/mycocktailbar/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.hitme.android.mycocktailbar
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -16,6 +19,23 @@ class MainApplication : Application() {
     lateinit var workManager: WorkManager
 
     override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .build()
+            )
+        }
         super.onCreate()
 
         val periodicWorkRequest = PeriodicWorkRequestBuilder<FavoritesUpdateWorker>(INTERVAL, TimeUnit.HOURS).build()
